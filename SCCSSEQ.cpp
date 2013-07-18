5bb2400814c77a00b8b5b536f324707695214d07
Toplus <toplus90@alice.it>
2013-07-18 10:51:29 +0200

    added comments to SCCSSEQ.cpp

--- SCCSSEQ.cpp
+++ SCCSSEQ.cpp
@@ -7,17 +7,24 @@
 #include "SCCSSEQ.h"
 #include <iostream>
 
+/**
+ * @brief 				Method for computing the the sequence of SCC sorted in a topological way
+ * @param gamma			the framework which contains the graph
+ * @retval 				a list of pointer to SCC
+ */
+
 list<SCC*> SCCSSEQ(AF gamma){
 	list<SCC*> list_SCC;
 	stack<DFS_node*> G = initialize_stack(gamma);
 	cout<<"DFS first start"<<endl;
+	// first call of DFS
 	DFS(G,true);
 	cout<<"DFS second start"<<endl;
-
+	// second call of DFS, considering edges in the opposite direction
 	DFS(G,false);
 	cout<<"DFS finish"<<endl;
 
-
+	// remove from the stack root DFS_node which are not root of the tree
 	DFS_node* u;
 	stack<DFS_node*> root = stack<DFS_node*>(G);
 	while(!G.empty()){
@@ -29,6 +36,8 @@ list<SCC*> SCCSSEQ(AF gamma){
 	}
 	cout<<"1* while finish"<<endl;
 	int k=0;
+
+	// create from every root the sequence of SCC
 	while(!root.empty()){//puntatori??
 		cout<<k++<<endl;
 		DFS_node* temp = new DFS_node();
@@ -46,6 +55,14 @@ list<SCC*> SCCSSEQ(AF gamma){
 	cout<<"2* while"<<endl;
 
 }
+
+/**
+ * @brief 				compute the removal of a node from the stack
+ * @param remove		node to remove
+ * @retval void
+ */
+
+
 void remove_stack(stack<DFS_node*> * S, DFS_node * remove){
 	stack<DFS_node*> temp;
 	while(!S->empty()){
@@ -59,6 +76,12 @@ void remove_stack(stack<DFS_node*> * S, DFS_node * remove){
 		temp.pop();
 	}
 }
+
+/**
+ * @brief 				sort of the stack
+ * @param S				stack which is to sort
+ * @retval 				the sorted stack
+ */
 
 
 stack<DFS_node*> sort_stack(stack<DFS_node*> S){//e azzero colore
@@ -79,9 +102,23 @@ stack<DFS_node*> sort_stack(stack<DFS_node*> S){//e azzero colore
 	return stack;
 }
 
+/**
+ * @brief 				Criterion used to compare two nodes: finish time
+ * @param a				first node to compare
+ * @param b				second node to compare
+ * @retval 				true if the first is greater then the second
+ */
+
 bool compareTime(DFS_node* a,DFS_node* b){
     return (a->f)>(b->f);
 }
+
+/**
+ * @brief 				depth first search
+ * @param S				Stack in which we have to do the DFS
+ * @param first			true if is the first time which we are using this method in the SCCSSEQ method
+ * @retval void
+ */
 
 void DFS(stack<DFS_node*> S, bool first){
 	list<SCC*> SCCSSEQ;
@@ -89,13 +126,15 @@ void DFS(stack<DFS_node*> S, bool first){
 	DFS_node* u;
 	SCC scc_tmp;
 
-	//copio la stack S, in modo tale che anche se estraiamo gli elementi da S, la stack G rimane inalterata	
+	//copy of S stack, in this way we can extract element from S, and G doesn't change
 	stack<DFS_node*> G = stack<DFS_node*>(S);
 
+	// if is the second time, we sort the stack
 	if(!first){
 		S=sort_stack(S);
 	}
 	int scc_num = 0;
+	// visit every node of the stack if the color is white
 	while(!S.empty()){
 		u = S.top();
 		S.pop();
@@ -111,8 +150,15 @@ void DFS(stack<DFS_node*> S, bool first){
 			cout << endl;
 		}
 	}
-
 }
+
+/**
+ * @brief 				visit in the depth first search
+ * @param u				node which we are visiting
+ * @param first			true if is the first time which we are using DFS in the SCCSSEQ method
+ * @retval SCC			return the SCC computed
+ */
+
 
 SCC DFS_visit(stack<DFS_node*> S, DFS_node* u, int* time, bool first){
 	SCC tmp_scc, tmp_scc2;
@@ -122,6 +168,7 @@ SCC DFS_visit(stack<DFS_node*> S, DFS_node* u, int* time, bool first){
 	//cout << (u->argument->getName())<<" set gray"<<endl;
 	DFS_node * temp;
 
+	// if is the first time we consider the attacked nodes, otherwise we consider the attackers ( way to compute G trasposed )
 	SetArguments * adj;
 	if(first){
 		adj = u->argument->get_attacks();
@@ -130,6 +177,7 @@ SCC DFS_visit(stack<DFS_node*> S, DFS_node* u, int* time, bool first){
 		adj = u->argument->get_attackers();
 	}
 
+	// if the color is white call DFS_visit, compute SCC by union 
 	for(SetArgumentsIterator it = adj->begin(); it != adj->end(); it++ ){
 		temp = get_DFS_node(S, **it );
 		//cout << (temp->argument->getName())<<" sub"<<endl;
@@ -152,6 +200,12 @@ SCC DFS_visit(stack<DFS_node*> S, DFS_node* u, int* time, bool first){
 	return tmp_scc;
 }
 
+/**
+ * @brief 				get a DFS_node if is present in the stack a node with the corresponding Argument
+ * @param a				argument which we are looking for
+ * @retval DFS_node*	pointer of the corresponding DFS_node if is presente in the stack, otherwise NULL
+ */
+
 DFS_node* get_DFS_node(stack<DFS_node*> S, Argument  a ){
 	DFS_node * u;
 	while(!S.empty()){
@@ -163,6 +217,12 @@ DFS_node* get_DFS_node(stack<DFS_node*> S, Argument  a ){
 	return NULL;
 }
 
+
+/**
+ * @brief 				initialization of the stack 
+ * @param gamma			framework
+ * @retval stack		corresponding stack of DFS_node initialized
+ */
 
 stack<DFS_node*> initialize_stack(AF gamma){
 
